3e607eb913afd8159c38c2eaff4b244553020efc
rashadibra <130538356+rashadibra@users.noreply.github.com>
2025-07-24 11:39:50 +0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import { Button } from "react-bootstrap";
+
+function App() {
+  return (
+    <>
+      <Button className="p-4 m-2 flex j-ce">click</Button>
+    </>
+  );
+}
+
+export default App;
